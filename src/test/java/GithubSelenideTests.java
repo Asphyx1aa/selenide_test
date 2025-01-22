5775f91cff120eb17acacf46950e834383c82a4b
Timur <tim.vlasov96@gmail.com>
2025-01-22 20:54:48 +0300

    added test #2

--- src/test/java/GithubSelenideTests.java
+++ src/test/java/GithubSelenideTests.java
@@ -11,16 +11,28 @@ public class GithubSelenideTests {
     static void setUpTest() {
         Configuration.baseUrl = "https://github.com/";
         Configuration.browserSize = "1920x1080";
+        Configuration.pageLoadStrategy = "eager";
     }
 
     @Test
     void SearchSoftAssertionInSelenideTest() {
-        open("https://github.com/");
-        $(".search-input").click();
-        $("[name=query-builder-test]").setValue("selenide").pressEnter();
-        $$("[data-testid=results-list]").first().find(byText("selenide")).click();
+        open("https://github.com/selenide/selenide");
         $("#wiki-tab").click();
         $(".js-wiki-more-pages-link").click();
         $(".js-wiki-sidebar-toggle-display").shouldHave(text("SoftAssertions"));
+        $(".js-wiki-sidebar-toggle-display").$(byText("SoftAssertions")).click();
+        $("#wiki-wrapper").shouldHave(text("""
+                @ExtendWith({SoftAssertsExtension.class})
+                class Tests {
+                  @Test
+                  void test() {
+                    Configuration.assertionMode = SOFT;
+                    open("page.html");
+                
+                    $("#first").should(visible).click();
+                    $("#second").should(visible).click();
+                  }
+                }
+                """));
     }
 }
